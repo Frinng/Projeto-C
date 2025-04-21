8c170d67c0bc7d9bab295963fdbb5e3113c90d85
Frinng <167120714+Frinng@users.noreply.github.com>
2025-04-20 22:08:27 -0300

    Atividade01

---
+++ SALogicadeprogramaçaõ/Atividade01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int n1;
+    int n2;
+    int result;
+
+    printf("Digite um numero: ");
+    scanf("%i", &n1);
+
+    printf("Digite um segundo numero: ");
+    scanf("%i", &n2);
+
+    
+    return 0;
+}
